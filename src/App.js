8ba28c611c38c391a5b9e79e35ebd204a0009acc
GokdenizCakir <gokdenizckr@gmail.com>
2023-11-03 15:09:52 +0300

    added logo inside

--- src/App.js
+++ src/App.js
@@ -1,61 +1,62 @@
-import "./App.css";
-import { SectionWrapper, Features, Download } from "./components";
+import './App.css';
+import { SectionWrapper, Features, Download } from './components';
 import styles from './styles/Global';
-import assets from "./assets";
+import assets from './assets';
+import logoapp from './assets/logoapp_withoutbackground.png';
 
 function App() {
   return (
     <>
       <SectionWrapper
-        title="Design Marketplace"
-        description="Where Innovation Meets Imagination"
+        title='Desginerio'
+        description='Where Innovation Meets Imagination'
         showBtn
         mockupImg={assets.homeHero}
-        banner="banner"
+        banner='banner'
       />
       <SectionWrapper
-        title="1. Redefine Design with Cutting-Edge Technology:"
-        description="Design Marketplace is a revolutionary startup project that leverages AI technology to bring designers and design clients together. It combines aesthetics and functionality to transform the design world."
+        title='1. Redefine Design with Cutting-Edge Technology:'
+        description='Designerio is a revolutionary startup project that leverages AI technology to bring designers and design clients together. It combines aesthetics and functionality to transform the design world.'
         mockupImg={assets.homeCards}
         reverse
       />
       <Features />
       <SectionWrapper
-        title="2. Diverse Design Categories:"
-        description="From fashion design to interior design, graphic design to web design, Design Marketplace brings together expert designers in a wide range of fields. Find the best professionals in the area you need."
+        title='2. Diverse Design Categories:'
+        description='From fashion design to interior design, graphic design to web design, Design Marketplace brings together expert designers in a wide range of fields. Find the best professionals in the area you need.'
         mockupImg={assets.feature}
         reverse
       />
       <SectionWrapper
-        title="3. Effective Communication and Collaboration:"
-        description="The platform provides effective communication and collaboration opportunities between designers and clients. Share your ideas, shape your projects, and bring them to life."
+        title='3. Effective Communication and Collaboration:'
+        description='The platform provides effective communication and collaboration opportunities between designers and clients. Share your ideas, shape your projects, and bring them to life.'
         mockupImg={assets.mockup}
-        banner="banner02"
-        
+        banner='banner02'
       />
       <SectionWrapper
-        title="4. Security and Privacy Are Our Priorities:"
-        description="User data and privacy are protected to the highest standards. Design Marketplace provides a secure and reliable environment."
+        title='4. Security and Privacy Are Our Priorities:'
+        description='User data and privacy are protected to the highest standards. Design Marketplace provides a secure and reliable environment.'
         mockupImg={assets.homeSec}
         reverse
       />
-      <Features />
+      <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+        <img src={logoapp} alt='logo' />
+      </div>
+
       <SectionWrapper
         title=" 'Reshaping the World of Design'
         Discover Design Marketplace and unlock the potential in your projects!"
-        description=""
+        description=''
         mockupImg={assets.homePeop}
         reverse
       />
-
-      
-      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+      <div className='px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04'>
         <p className={`${styles.pText} ${styles.whiteText}`}>
-          Code to <span className="bold">SkyLab</span>
+          Code to <span className='bold'>SkyLab</span>
         </p>
       </div>
     </>
   );
-};
+}
 
 export default App;
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,19 +1,61 @@
-import React from "react";
-import assets from "../assets";
-import styles from "../styles/Global";
-import Button from "./Button";
+import React from 'react';
+import assets from '../assets';
+import styles from '../styles/Global';
+import Button from './Button';
 
-const SectionWrapper = ({ title, description, showBtn, mockupImg, banner,reverse }) => {
+const SectionWrapper = ({
+  title,
+  description,
+  showBtn,
+  mockupImg,
+  banner,
+  reverse,
+}) => {
   return (
-    <div className={`min-h-screen ${styles.section} ${reverse? styles.bgWhite : styles.bgPrimary} ${banner}`}>
-      <div className={`flex items-center w-11/12 sm:w-full md:w-3/4 ${reverse? styles.boxReverseClass :styles.boxClass }`}>
-        <div className={`${styles.descDiv} ${reverse? "fadeRightMini" :"fadeLeftMini"} ${reverse?styles.textRight:styles.textLeft}`}>
-          <h1 className={`${styles.h1Text} ${reverse? styles.blackText:styles.whiteText}`}>{title}</h1>
-          <p className={`${styles.descriptionText}  ${reverse? styles.blackText:styles.whiteText}`}>{description}</p>
-          {showBtn && <Button assetUrl={assets.expo} link="https://www.yildizskylab.com/"/>}
+    <div
+      className={`min-h-screen ${styles.section} ${
+        reverse ? styles.bgWhite : styles.bgPrimary
+      } ${banner}`}
+    >
+      <div
+        className={`flex items-center w-11/12 sm:w-full md:w-3/4 ${
+          reverse ? styles.boxReverseClass : styles.boxClass
+        }`}
+      >
+        <div
+          className={`${styles.descDiv} ${
+            reverse ? 'fadeRightMini' : 'fadeLeftMini'
+          } ${reverse ? styles.textRight : styles.textLeft}`}
+        >
+          <h1
+            className={`${styles.h1Text} ${
+              reverse ? styles.blackText : styles.whiteText
+            }`}
+          >
+            {title}
+          </h1>
+          <p
+            className={`${styles.descriptionText}  ${
+              reverse ? styles.blackText : styles.whiteText
+            }`}
+          >
+            {description}
+          </p>
+          {showBtn && (
+            <Button
+              assetUrl={assets.expo}
+              link='https://www.yildizskylab.com/'
+            />
+          )}
         </div>
         <div className={`flex-1 ${styles.flexCenter} p-8 sm:px-0`}>
-            <img src={mockupImg} alt="mockup" className={`${reverse? "fadeLeftMini" :"fadeRightMini"} ${styles.sectionImg}`} />
+          <img
+            src={mockupImg}
+            alt='mockup'
+            className={`${reverse ? 'fadeLeftMini' : 'fadeRightMini'} ${
+              styles.sectionImg
+            }`}
+          />
         </div>
       </div>
     </div>
